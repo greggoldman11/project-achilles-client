f702da0130f48ef461987b06f4e66c0626068a74
greg goldman <gregory.goldman97@gmail.com>
2021-07-27 10:27:07 -0400

    Update the styling of the home page
    This updates the styling of the homepage to add a shadow and flex
    styling to the content

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Button from 'react-bootstrap/Button'
+import './home.scss'
 
 const Home = () => {
   const [about, setAbout] = useState(false)
+  const aboutRef = useRef()
+  const topRef = useRef()
+  function aboutClicked () {
+    setAbout(!about)
+  }
+
+  useEffect(() => {
+    if (about) {
+      aboutRef.current.scrollIntoView()
+    }
+  }, [about])
+
   const aboutJSX =
   <div>
-    <h2>About Section</h2>
     <p>The story of Project Achilles:</p>
-    <section>
-    Homer&apos;s <i>Iliad</i> tells the stroy of Achilles, the greatest warrior in all of Greece during the Trojan War.
-    He was invincible everywhere, except for on his heel. After being struck with an arrow to the ankle Achilles perished.
-
-    Project Achilles was created out of the understanding that although most Veterans have access to many resources, some do not.
-    Therefore, it was created to help bring resources to those who have less access.
-
-    Through Project Achilles Veterans can come together and share some of their favorite reources, and help others get much needed
-    assistance.
-    </section>
+    <div className="indented">
+      <p>
+        Homer&apos;s <i>Iliad</i> tells the stroy of Achilles, the greatest warrior in all of Greece during the Trojan War.
+        He was invincible everywhere, except for on his heel. After being struck with an arrow to the ankle Achilles perished.
+      </p>
+      <p>
+        Project Achilles was created out of the understanding that although most Veterans have access to many resources, some do not.
+        Therefore, it was created to help bring resources to those who have less access.
+        Through Project Achilles Veterans can come together and share some of their favorite reources, and help others get much needed
+        assistance.
+      </p>
+    </div>
   </div>
   return (
-    <div className="flex-container">
-      <h1 className="flex-item home">Project Achilles</h1>
-      <section className="flex-item home">{about ? aboutJSX : ''}
-      </section>
-      <Button onClick={() => setAbout(!about)}>{about ? 'Close' : 'About'}</Button>
-    </div>
+    <section className="aboutSection">
+      <div className="container" ref={topRef}>
+        <div className="header-row row">
+          <div className="header-row-text row">
+          Project Achilles
+          </div>
+        </div>
+        <div className="row" ref={aboutRef}>
+          <section className={about ? 'show-class' : ''}>
+            {about ? aboutJSX : ''}
+          </section>
+        </div>
+        <div className="row">
+          <Button onClick={aboutClicked} className={about ? 'show-button' : ''}>{about ? 'Close' : 'About'}</Button>
+        </div>
+      </div>
+    </section>
   )
 }
 
